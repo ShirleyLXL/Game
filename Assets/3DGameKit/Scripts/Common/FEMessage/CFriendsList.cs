1987b07aa8b58ff052515b96736ab0fb7cb3dd0d
Shirley <33152089+ShirleyLXL@users.noreply.github.com>
2018-12-13 23:18:36 +0800

    Update CFriendsList.cs

--- Assets/3DGameKit/Scripts/Common/FEMessage/CFriendsList.cs
+++ Assets/3DGameKit/Scripts/Common/FEMessage/CFriendsList.cs
@@ -6,6 +6,6 @@ namespace Common
     public class CFriendsList : Message
     {
         public CFriendsList() : base(Command.C_FRIENDS_LIST) { }
-        public int userID;    //这个消息可以用专门发送userID的消息来代替
+        public int userID;      //只发送userID
     }
 }
